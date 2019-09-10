0d1826174429d099fa65bb17d1d00f2afb7a785d
shilpa <shilparpatil86@gmail.com>
2019-09-10 17:04:22 +0530

    First assignment

---
+++ Assign1.py
@@ -0,0 +1,9 @@
+s = "Python for life"
+print(s[0:6])
+print(s[7:10])
+print(s[11:])
+print(s[2:10])
+print(s[2:10])
+print(s[8:13])
+print(s[0:10], s[11:].upper())
+print(s[0:6].upper(), s[7:10], s[11:].upper())
